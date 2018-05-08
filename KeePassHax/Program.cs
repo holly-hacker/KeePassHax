3967cc324179c63966fc1d068336f418b62df0e4
HoLLy <dario.db@hotmail.com>
2018-05-08 12:57:34 +0200

    Add project files.

---
+++ KeePassHax/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KeePassHax
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
